e6ec147e614b6b14b358f20ddb1ba40cbf66c38f
void4main <void@mailbox.org>
2025-10-03 00:59:05 +0200

    Add: EguiScrollArea

--- src/plugin_egui.rs
+++ src/plugin_egui.rs
@@ -52,9 +52,7 @@ fn ui_system(mut contexts: EguiContexts, read: Res<ShareStruct>, mut ui_state: R
                 &mut ui_state.pos_ground_projection,
                 "Project position to ground",
             );
-            ui.checkbox(
-                &mut ui_state.pos_ground_arrow,
-                "Arrow position to ground");
+            ui.checkbox(&mut ui_state.pos_ground_arrow, "Arrow position to ground");
         });
 
         // Statistics section
@@ -97,7 +95,9 @@ fn ui_system(mut contexts: EguiContexts, read: Res<ShareStruct>, mut ui_state: R
         egui::CollapsingHeader::new(heading)
             .default_open(true)
             .show(ui, |ui| {
+                egui::ScrollArea::vertical().show(ui, |ui| {
                 egui::Grid::new("some_unique_id").show(ui, |ui| {
+
                     // Headline
                     ui.centered_and_justified(|ui| {
                         ui.label(RichText::new("HEX").strong());
@@ -110,125 +110,133 @@ fn ui_system(mut contexts: EguiContexts, read: Res<ShareStruct>, mut ui_state: R
                     }
                     ui.end_row();
 
-                    // List of planes
-                    for plane_id in plane_list.clone() {
-                        // Statistics
-                        ui_state.plane_ids.insert(plane_id.to_string());
+                        // List of planes
+                        for plane_id in plane_list.clone() {
+                            // Statistics
+                            ui_state.plane_ids.insert(plane_id.to_string());
 
-                        // Squawk
-                        let squawk;
-                        let mut squawk_str = "-".to_string();
-                        let mut color = Color32::GRAY;
-                        if let Some(squawk_tmp) = read_tmp.get_squawk(plane_id.to_string()) {
-                            squawk = squawk_tmp;
-                            if let Some(squawk) = get_transponder_description(squawk) {
-                                color = squawk.1.to_color32();
+                            // Squawk
+                            let squawk;
+                            let mut squawk_str = "-".to_string();
+                            let mut color = Color32::GRAY;
+                            if let Some(squawk_tmp) = read_tmp.get_squawk(plane_id.to_string()) {
+                                squawk = squawk_tmp;
+                                if let Some(squawk) = get_transponder_description(squawk) {
+                                    color = squawk.1.to_color32();
+                                }
+                                squawk_str = squawk_tmp.to_string();
                             }
-                            squawk_str = squawk_tmp.to_string();
-                        }
 
-                        // Height level
-                        let height_level = read_tmp.get_latest_known_altitude(plane_id.to_string()).map(|alt| alt.to_string()).unwrap_or("-".to_string());
+                            // Height level
+                            let height_level = read_tmp
+                                .get_latest_known_altitude(plane_id.to_string())
+                                .map(|alt| alt.to_string())
+                                .unwrap_or("-".to_string());
 
-                        // Update statistics
-                        if let Some(height_level) = read_tmp
-                            .get_latest_known_pos(plane_id.to_string())
-                            .map(|pos| pos.2)
-                        {
-                            ui_state.max_height_level = ui_state.max_height_level.max(height_level);
-                            ui_state.min_height_level = Some(
-                                ui_state
-                                    .min_height_level
-                                    .map_or(height_level, |min| min.min(height_level)),
+                            // Update statistics
+                            if let Some(height_level) = read_tmp
+                                .get_latest_known_pos(plane_id.to_string())
+                                .map(|pos| pos.2)
+                            {
+                                ui_state.max_height_level =
+                                    ui_state.max_height_level.max(height_level);
+                                ui_state.min_height_level = Some(
+                                    ui_state
+                                        .min_height_level
+                                        .map_or(height_level, |min| min.min(height_level)),
+                                );
+                            }
+
+                            // Speed over ground
+                            let ground_speed = read_tmp
+                                .get_ground_speed(plane_id.to_string())
+                                .map(|speed| speed.to_string())
+                                .unwrap_or("-".to_string());
+
+                            if let Some(ground_speed) =
+                                read_tmp.get_ground_speed(plane_id.to_string())
+                            {
+                                if ground_speed > ui_state.max_speed {
+                                    ui_state.max_speed = ground_speed;
+                                }
+                                if ground_speed < ui_state.min_speed.map_or(1000.0, |value| value) {
+                                    ui_state.min_speed = Some(ground_speed);
+                                }
+                            }
+
+                            // Track
+                            let track = read_tmp
+                                .get_track(plane_id.to_string())
+                                .map(|t| t.to_string())
+                                .unwrap_or("-".to_string());
+
+                            // Call sign
+                            let call_sign = read_tmp.get_call_sign(plane_id.to_string());
+
+                            // Is on ground
+                            // let on_ground_str = read_tmp
+                            //     .is_on_ground(plane_id.to_string())
+                            //     .filter(|&is_on_ground| is_on_ground)
+                            //     .map(|_| "on ground".to_string())
+                            //     .unwrap_or("-".to_string());
+
+                            // Vertical rate
+                            let vertical_rate = read_tmp.get_vertical_rate(plane_id.to_string());
+
+                            // Update statistics
+                            if vertical_rate.is_some() {
+                                ui_state.min_vertical_rate =
+                                    ui_state.min_vertical_rate.min(vertical_rate.unwrap());
+                                ui_state.max_vertical_rate =
+                                    ui_state.max_vertical_rate.max(vertical_rate.unwrap());
+                            }
+
+                            let vertical_rate_str = vertical_rate
+                                .map(|t| t.to_string())
+                                .unwrap_or("-".to_string());
+
+                            // Distance to antenna
+                            // Antennenposition 53.5718392,9.9834842
+                            // TODO: Fix static setup
+                            let dist_to_antenna = read_tmp
+                                .get_plane_distance_to_lat_lon(
+                                    plane_id.to_string(),
+                                    53.5718392,
+                                    9.9834842,
+                                )
+                                .unwrap_or(0.0);
+                            let dist_to_antenna_str = if dist_to_antenna == 0.0 {
+                                "-".to_owned()
+                            } else {
+                                format!("{:05.1}", dist_to_antenna)
+                            };
+
+                            // Update statistics
+                            if dist_to_antenna > ui_state.max_distance_to_antenna {
+                                ui_state.max_distance_to_antenna = dist_to_antenna.clone();
+                            }
+
+                            let checkbox_value = ui_state.selected(plane_id);
+
+                            // Build row
+                            ui.checkbox(
+                                checkbox_value,
+                                RichText::new(plane_id.to_string()).color(Color32::LIGHT_RED),
                             );
-                        }
-
-                        // Speed over ground
-                        let ground_speed = read_tmp
-                            .get_ground_speed(plane_id.to_string())
-                            .map(|speed| speed.to_string())
-                            .unwrap_or("-".to_string());
-
-                        if let Some(ground_speed) = read_tmp.get_ground_speed(plane_id.to_string())
-                        {
-                            if ground_speed > ui_state.max_speed {
-                                ui_state.max_speed = ground_speed;
-                            }
-                            if ground_speed < ui_state.min_speed.map_or(1000.0, |value| value) {
-                                ui_state.min_speed = Some(ground_speed);
-                            }
-                        }
-
-                        // Track
-                        let track = read_tmp
-                            .get_track(plane_id.to_string())
-                            .map(|t| t.to_string())
-                            .unwrap_or("-".to_string());
-
-                        // Call sign
-                        let call_sign = read_tmp.get_call_sign(plane_id.to_string());
-
-                        // Is on ground
-                        // let on_ground_str = read_tmp
-                        //     .is_on_ground(plane_id.to_string())
-                        //     .filter(|&is_on_ground| is_on_ground)
-                        //     .map(|_| "on ground".to_string())
-                        //     .unwrap_or("-".to_string());
-
-                        // Vertical rate
-                        let vertical_rate = read_tmp
-                            .get_vertical_rate(plane_id.to_string());
-
-                        // Update statistics
-                        if vertical_rate.is_some() {
-                            ui_state.min_vertical_rate = ui_state.min_vertical_rate.min(vertical_rate.unwrap());
-                            ui_state.max_vertical_rate = ui_state.max_vertical_rate.max(vertical_rate.unwrap());
-                        }
-                        
-                        let vertical_rate_str = vertical_rate.map(|t| t.to_string())
-                            .unwrap_or("-".to_string());
-
-                        // Distance to antenna
-                        // Antennenposition 53.5718392,9.9834842
-                        // TODO: Fix static setup
-                        let dist_to_antenna = read_tmp
-                            .get_plane_distance_to_lat_lon(
-                                plane_id.to_string(),
-                                53.5718392,
-                                9.9834842,
-                            )
-                            .unwrap_or(0.0);
-                        let dist_to_antenna_str = if dist_to_antenna == 0.0 {
-                            "-".to_owned()
-                        } else {
-                            format!("{:05.1}", dist_to_antenna)
+                            //ui.label(plane_id);
+                            ui.label(RichText::new(squawk_str).color(color));
+                            ui.label(height_level);
+                            ui.label(vertical_rate_str);
+                            //ui.label(vertical_rate_simple_str);
+                            ui.label(ground_speed);
+                            ui.label(track);
+                            ui.label(call_sign);
+                            // ui.label(on_ground_str);
+                            ui.label(dist_to_antenna_str);
+                            ui.end_row();
                         };
 
-                        // Update statistics
-                        if dist_to_antenna > ui_state.max_distance_to_antenna {
-                            ui_state.max_distance_to_antenna = dist_to_antenna.clone();
-                        }
-
-                        let checkbox_value = ui_state.selected(plane_id);
-
-                        // Build row
-                        ui.checkbox(
-                            checkbox_value,
-                            RichText::new(plane_id.to_string()).color(Color32::LIGHT_RED),
-                        );
-                        //ui.label(plane_id);
-                        ui.label(RichText::new(squawk_str).color(color));
-                        ui.label(height_level);
-                        ui.label(vertical_rate_str);
-                        //ui.label(vertical_rate_simple_str);
-                        ui.label(ground_speed);
-                        ui.label(track);
-                        ui.label(call_sign);
-                        // ui.label(on_ground_str);
-                        ui.label(dist_to_antenna_str);
-                        ui.end_row();
-                    }
                 });
-            });
+            });});
     });
 }
